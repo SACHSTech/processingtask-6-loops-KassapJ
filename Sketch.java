c7b27bc091c129e7afea16e333930ad1d13f5864
John Matthew Kassapian <johnmatthew.kassapian23@ycdsbk12.ca>
2022-01-07 16:25:22 -0500

    swaggy

--- Sketch.java
+++ Sketch.java
@@ -8,7 +8,7 @@ public class Sketch extends PApplet {
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(600, 600);
   }
 
   /** 
@@ -16,7 +16,7 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(255);
   }
 
   /**
@@ -24,12 +24,39 @@ public class Sketch extends PApplet {
    */
   public void draw() {
 	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+	// Quadrant 1
+  stroke(0);
+  strokeWeight(width / 120);
+  for(int lineX = ((width / 2) / 10); lineX < width / 2; lineX += ((width / 2) / 10)){
+    line(lineX, 0, lineX, height / 2);
+  }
+  for(int lineY = ((height / 2) / 10); lineY < height / 2; lineY += ((height / 2) / 10)){
+    line(0, lineY, width / 2, lineY);
+  }
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+  // Quadrant 2
+  fill(225, 15, 125);
+  strokeWeight(width / 300);
+  
+  for(int i = 0; i < 5; i++){
+    int Y = i * ((height / 2) / 5) + height / 20;
+    for(int j = 0; j < 5; j++){
+      int X = j * ((width / 2) / 5) + (width / 2) + width / 20;
+
+      ellipse(X, Y, width / 17, height / 17);
+    }
+
+  }
+
+  //Quadrant 3
+  strokeWeight(1);
+  int color = 0;
+  for(int i = 0; i <= width / 2; i++){
+    stroke(color);
+    line(i, height / 2, i, height);
+    color += (255 / (width / 2));
+  }
+
   }
   
   // define other methods down here.
